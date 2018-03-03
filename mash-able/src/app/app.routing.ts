fc9f0fe916e950d82ea4a6c35c3a0e05f8bbee35
Pedro Henrique <pedrohferreir@hotmail.com>
2018-03-03 17:33:57 -0300

    inicio

---
+++ mash-able/src/app/app.routing.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+import { AdminLayoutComponent } from './layouts/admin/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
+
+export const AppRoutes: Routes = [{
+  path: '',
+  component: AdminLayoutComponent,
+  children: [
+    {
+      path: 'simple-page',
+      loadChildren: './simple-page/simple-page.module#SimplePageModule'
+    }
+  ]
+}, {
+  path: '',
+  component: AuthLayoutComponent,
+  children: [{
+    path: 'error',
+    loadChildren: './error/error.module#ErrorModule'
+  },{
+    path: 'landing',
+    loadChildren: './landing/landing.module#LandingModule'
+  },]
+}, {
+  path: '**',
+  redirectTo: 'error/404'
+}];
---
+++ mash-able/src/app/shared/menu-items/menu-items.ts
@@ -0,0 +1,55 @@
+import {Injectable} from '@angular/core';
+
+export interface BadgeItem {
+    type: string;
+    value: string;
+}
+
+export interface ChildrenItems {
+    state: string;
+    target?: boolean;
+    name: string;
+    type?: string;
+    children?: ChildrenItems[];
+}
+
+export interface MainMenuItems {
+    state: string;
+    main_state?: string;
+    target?: boolean;
+    name: string;
+    type: string;
+    icon: string;
+    badge?: BadgeItem[];
+    children?: ChildrenItems[];
+}
+
+export interface Menu {
+    label: string;
+    main: MainMenuItems[];
+}
+
+const MENUITEMS = [
+    {
+        label: 'Other',
+        main: [
+            {
+                state: 'simple-page',
+                name: 'Simple Page',
+                type: 'link',
+                icon: 'ti-layout-sidebar-left'
+            }
+        ]
+    }
+];
+
+@Injectable()
+export class MenuItems {
+    getAll(): Menu[] {
+        return MENUITEMS;
+    }
+
+    /*add(menu: Menu) {
+      MENUITEMS.push(menu);
+    }*/
+}
