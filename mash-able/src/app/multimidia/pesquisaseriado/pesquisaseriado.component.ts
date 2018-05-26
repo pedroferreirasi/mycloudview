96b06740e5cac11474b19f77add1c13a68efb345
Pedro Henrique <pedroferreirasi@gmail.com>
2018-05-26 13:38:22 -0300

    segunda versao da lista generica

--- mash-able/src/app/multimidia/pesquisaseriado/pesquisaseriado.component.ts
+++ mash-able/src/app/multimidia/pesquisaseriado/pesquisaseriado.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
-import { DatatableComponent } from "@swimlane/ngx-datatable";
-import { Seriado } from '../../model/multimidia/seriado.model'; 
+import { ListaGenerica } from '../../commom/framework/listagenerica';
 import { URL_SERIADO } from '../../commom/constante/constantes';
 
 @Component({
@@ -10,80 +9,13 @@ import { URL_SERIADO } from '../../commom/constante/constantes';
   templateUrl: './pesquisaseriado.component.html',
   styleUrls: ['./pesquisaseriado.component.css']
 })
-export class PesquisaSeriadoComponent implements OnInit {
-  
-  private registrosPorPagina: number = 10;
-  private dataSource = [];
-  private tempDataSource = [];
-  private registroSelecionado = [];  
+export class PesquisaSeriadoComponent extends ListaGenerica implements OnInit {
 
-  @ViewChild(DatatableComponent) table: DatatableComponent;
 
   constructor() {
-    this.getAll((data) => {
-      this.registroSelecionado = [data[0]];
-      this.dataSource = data;
-      this.tempDataSource = [...data];
-    });
+    super(URL_SERIADO);
   }
 
   ngOnInit() { }
 
-  getAll(cb) {
-    const req = new XMLHttpRequest();
-    req.open('GET',`${URL_SERIADO}`);
-
-    req.onload = () => {
-      cb(JSON.parse(req.response));
-    };
-
-    req.send();
-  }
-
-  alterarLimit(evento: KeyboardEvent) {
-    this.registrosPorPagina = parseInt((<HTMLInputElement>evento.target).value);
-    this.table.limit = this.registrosPorPagina;
-    this.table.recalculate();
-  }
-
-  filtrar(event) {
-
-    let rowsPesquisa = [];
-    let rowsFiltro = [];
-
-    rowsFiltro = this.tempDataSource;
-
-    // Valor digitado para pesquisa
-    let valorPesquisa = event.target.value.toLowerCase();
-
-    // Contador de quantas colunas existem na tabela
-    let quantidadeColunasTabela = this.table.columns.length;
-
-    // Array dos nomes das colunas da tabela
-    let colunasTabela = Object.keys(rowsFiltro[0]);
-
-    rowsPesquisa = rowsFiltro.filter(function (item) {
-      // Pesquisa em todas as colunas
-      for (let i = 0; i < quantidadeColunasTabela; i++) {
-        if (typeof item[colunasTabela[i]] == "string") {
-          if ((<string>item[colunasTabela[i]]).toLowerCase().indexOf(valorPesquisa) !== -1 || !valorPesquisa) {
-            return true;
-          }
-        }
-      }
-    });
-
-    if (rowsPesquisa.length > 0) {
-      this.dataSource = rowsPesquisa;
-    } else {
-      this.dataSource = [];
-    }
-
-    // whenever the filter changes, always go back to the first page
-    this.table.offset = 0;
-  }
-
-  onSelect({ registroSelecionado }) { }
-
-  onActivate(event) { }
 }
