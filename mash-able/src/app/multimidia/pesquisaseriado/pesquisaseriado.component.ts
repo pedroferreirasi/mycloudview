894b05fb0f6a7be022e60ccd9961930fbe12d5ac
Pedro Henrique <pedroferreirasi@gmail.com>
2018-05-26 12:50:42 -0300

    finalizacao da tela de listagem de seriado

--- mash-able/src/app/multimidia/pesquisaseriado/pesquisaseriado.component.ts
+++ mash-able/src/app/multimidia/pesquisaseriado/pesquisaseriado.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Http, Response } from '@angular/http';
 
 import { DatatableComponent } from "@swimlane/ngx-datatable";
-import { Seriado } from '../../model/multimidia/seriado.model';
+import { Seriado } from '../../model/multimidia/seriado.model'; 
+import { URL_SERIADO } from '../../commom/constante/constantes';
 
 @Component({
   selector: 'pesquisa-seriado',
@@ -10,35 +11,27 @@ import { Seriado } from '../../model/multimidia/seriado.model';
   styleUrls: ['./pesquisaseriado.component.css']
 })
 export class PesquisaSeriadoComponent implements OnInit {
-
-  public rowsOnPage: number = 10;
+  
+  private registrosPorPagina: number = 10;
+  private dataSource = [];
+  private tempDataSource = [];
+  private registroSelecionado = [];  
 
   @ViewChild(DatatableComponent) table: DatatableComponent;
-  _limitNumber : number;
-
-  rows = [];
-  rowsAux = [];
-
-  selected = [];
-
-  temp = [];
-
-  columns: any[] = [];
 
   constructor() {
-    this.fetch((data) => {
-      this.selected = [data[0]];
-      this.temp = [...data];
-      this.rows = data;
-      this.rowsAux = data;
+    this.getAll((data) => {
+      this.registroSelecionado = [data[0]];
+      this.dataSource = data;
+      this.tempDataSource = [...data];
     });
   }
 
   ngOnInit() { }
 
-  fetch(cb) {
+  getAll(cb) {
     const req = new XMLHttpRequest();
-    req.open('GET', `http://pedroferreirasi.servehttp.com:8080/seriado/`);
+    req.open('GET',`${URL_SERIADO}`);
 
     req.onload = () => {
       cb(JSON.parse(req.response));
@@ -48,26 +41,49 @@ export class PesquisaSeriadoComponent implements OnInit {
   }
 
   alterarLimit(evento: KeyboardEvent) {
-    this.rowsOnPage = parseInt((<HTMLInputElement>evento.target).value);
-    this.table.limit = this.rowsOnPage;
+    this.registrosPorPagina = parseInt((<HTMLInputElement>evento.target).value);
+    this.table.limit = this.registrosPorPagina;
     this.table.recalculate();
   }
 
-  updateFilter(event) {
-    const val = event.target.value.toLowerCase();
+  filtrar(event) {
 
-    // filter our data    
-    const temp = this.temp.filter(function(d) {
-      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    let rowsPesquisa = [];
+    let rowsFiltro = [];
+
+    rowsFiltro = this.tempDataSource;
+
+    // Valor digitado para pesquisa
+    let valorPesquisa = event.target.value.toLowerCase();
+
+    // Contador de quantas colunas existem na tabela
+    let quantidadeColunasTabela = this.table.columns.length;
+
+    // Array dos nomes das colunas da tabela
+    let colunasTabela = Object.keys(rowsFiltro[0]);
+
+    rowsPesquisa = rowsFiltro.filter(function (item) {
+      // Pesquisa em todas as colunas
+      for (let i = 0; i < quantidadeColunasTabela; i++) {
+        if (typeof item[colunasTabela[i]] == "string") {
+          if ((<string>item[colunasTabela[i]]).toLowerCase().indexOf(valorPesquisa) !== -1 || !valorPesquisa) {
+            return true;
+          }
+        }
+      }
     });
 
-    // update the rows
-    this.table.rows = temp;
-    // Whenever the filter changes, always go back to the first page
+    if (rowsPesquisa.length > 0) {
+      this.dataSource = rowsPesquisa;
+    } else {
+      this.dataSource = [];
+    }
+
+    // whenever the filter changes, always go back to the first page
     this.table.offset = 0;
   }
 
-  onSelect({ selected }) { }
+  onSelect({ registroSelecionado }) { }
 
-  onActivate(event) {}
+  onActivate(event) { }
 }
